2056a667cdf111573cdf1283e77fcb3f849c3c10
Angus Bower-Brown <angusbowerbrown@protonmail.com>
2023-05-04 18:09:32 +0100

    commit tree 22e4d11249e2ef99297d076a5dd10fb3940b4ca5

--- lib/balancing_parentheses.rb
+++ lib/balancing_parentheses.rb
@@ -1,3 +1,22 @@
-require_relative './stack'
+require_relative 'stack.rb'
 
-# your code here
+def balancing_parentheses str
+    
+    stack = Stack.new
+
+    str.each_char do |char|
+
+        case char
+
+        when "("
+            stack.push(char)
+        when ")"
+            if stack.peek == "("
+                stack.pop
+            else
+                stack.push(char)
+            end
+        end
+    end
+    stack.size
+end
--- lib/stack.rb
+++ lib/stack.rb
@@ -1,23 +1,35 @@
 class Stack
 
-  def initialize
-    @stack = []
-  end
+    attr_reader :stack
 
-  def push(value)
-    @stack.push(value)
-  end
+    attr_reader :limit
 
-  def pop
-    @stack.pop
-  end
+    def initialize limit = nil
+        @stack = []
+        @limit = limit
+    end
 
-  def peek
-    @stack.last
-  end
+    def push element
+        if limit && stack.length >= limit
+            raise "Stack Overflow"
+        end
+        stack.push(element)
+    end
 
-  def size
-    @stack.length
-  end
+    def pop
+        stack.pop
+    end
 
+    def peek
+      # top of the stack LAST IN
+        stack.last
+    end
+
+    def size
+        stack.length
+    end
+
+    def empty?
+      stack.empty?
+    end
 end
